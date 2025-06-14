db5ef2800705b8a5670bbbc7140bb05140917185
Rcastro89 <r_castro_m@yahoo.es>
2025-06-14 14:06:32 -0500

    Create proyect

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="page-container">
+      <h1>Wompi Checkout</h1>
+      <p>Bienvenido al flujo de compra</p>
+    </div>
+  );
+}
+
+export default App
